0ed3aea4bfed9bc7ded859b3f761c9e6c906e346
chrishawnm <45526483+chrishawnm@users.noreply.github.com>
2020-10-11 17:04:00 -0400

    Update Census.py

--- Census.py
+++ Census.py
@@ -15,15 +15,19 @@ from geopy.geocoders import Nominatim
 import numpy as np
 import json
 
+#end user to choose a state in the United States
 state_input = input('Choose a state: ').title()
 
+#need for geopy api
 geolocator = Nominatim(user_agent="my-apps")
 
+#for heatmap
 plt.style.use('seaborn-colorblind')
 
 #add you API key
 YOUR_KEY = ''
 
+#api request 
 data = requests.get(
     'https://api.census.gov/data/2018/acs/acs5/profile?get='  
     + 'NAME,'       # State, County 
@@ -39,17 +43,24 @@ data = requests.get(
     + 'for=county:*&in=state:*'     
     + '&key=' + YOUR_KEY)
 
+#converting data to readable format (it converts it to a string)
 data = data.text
 
+#converts it to a list of list
 x = json.loads(data)
+
+#retrieving the header and then the body for pandas dataframe
 headers = x[0]
 data = x[1:]
 
+#convert lists to pandas dataframe 
 df = pd.DataFrame(data, columns=headers)
 
+#splitting column into state and county
 df['state'] = df['NAME'].str.split(', ').str[1]
 df['county'] = df['NAME'].str.split(',').str[0]
 
+#based on the state the end user has chose this will filter the dataset 
 df = df[df['state'] == state_input]
 
 #replacing value due to API not recognizing original value
@@ -57,21 +68,28 @@ if state_input == 'California':
     df['NAME'] = df['NAME'].replace('San Francisco County, California', 
                                     'San Francisco, California')
 
+#retrieving lon and lat to be graphed on heatmap
+print('Retrieving longitude and latitude please wait...')
 df['city_coord'] = df['NAME'].apply(geolocator.geocode)
 df = df.dropna(subset=['city_coord'])
 df['longitude'] = df['city_coord'].apply(lambda x: x.latitude)
 df['latitude'] = df['city_coord'].apply(lambda x: x.longitude)
+print('Finished retrieving longituden and latitude')
 
+#removing columns that are no longer needed
 df = df.drop(['NAME','city_coord'], axis=1) 
 
+# renaming columns
 df.columns = ['Average_Income','Population_Estimate','Enrolled_College',
               'Foreigner','Computer_Internet_Use','Health_Insurance',
               'Male_Pop','Female_Pop','Average_Family_Size','State','County',
               'Longitude','Latitude']
 
+#converting the first eight columns in the dataframe to int
 int_columns = df.columns.to_list()[:8]
 df[int_columns] = df[int_columns].astype(int)
 
+#converting column to a float
 df['Average_Family_Size'] = df['Average_Family_Size'].astype(float)
 
 # Average Income vs Population Estimate
@@ -109,3 +127,5 @@ plt.show(df.plot(kind ="scatter",
                  c=df['Computer_Internet_Use'],
                  cmap = plt.get_cmap('jet'),
                  colorbar=True))
+ 
+print('Graphs have been completed!')
